79b0e52886176a4571463df75c8da40804e68b03
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2018-12-03 22:26:09 +0300

    Add new solution

--- stack.py
+++ stack.py
@@ -2,10 +2,20 @@
 
 class ExtendedStack(list):
     def sum(self):
+        self.append(self.pop() + self.pop())
 
     def sub(self):
+        self.append(self.pop() - self.pop())
 
     def mul(self):
+        self.append(self.pop() * self.pop())
 
     def div(self):
+        self.append(self.pop() // self.pop())
+
+myStack = ExtendedStack()
+myStack.append(3)
+myStack.append(2)
+myStack.sub()
+print(myStack)
 
